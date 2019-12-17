34f2f0aa85b15bc17668e70ab79cb85e5349e63e
saurav <saurav@BRIMACLAP055.lan>
2019-12-17 09:19:50 +0530

    demo

--- datapipe/etl.py
+++ datapipe/etl.py
@@ -10,6 +10,7 @@ target_file_name = 'semantic_layer/final_data.csv'
 # Create a blob client using the local file name as the name for the blob
 blob_downlaod_client = blob_service_client.get_blob_client(container=container_name, blob=source_file_name)
 
+
 with open("./BlockDestination.txt", "wb") as my_blob:
     blob_data = blob_downlaod_client.download_blob()
     blob_data.readinto(my_blob)
