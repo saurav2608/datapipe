3197fa5cadf55dcd440e4097c00bb5062ba7225c
saurav <saurav@BRILAP1428.BRILLIO.COM>
2020-01-21 11:43:44 +0530

    minor changes

--- datapipe/etl.py
+++ datapipe/etl.py
@@ -5,7 +5,7 @@ connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 container_name = 'data'
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 source_file_name = 'landing_zone/raw_data.csv'
-target_file_name = 'semantic_layer/final_data_1.csv'
+target_file_name = 'semantic_layer/final_data.csv'
 
 # Create a blob client using the local file name as the name for the blob
 blob_downlaod_client = blob_service_client.get_blob_client(container=container_name, blob=source_file_name)
