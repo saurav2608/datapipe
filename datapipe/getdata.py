56b096cad0a8eaf2fdda65c8115b9d02995bc6ba
saurav <saurav@BRIMACLAP055.lan>
2020-01-21 14:48:54 +0530

    changes

--- datapipe/getdata.py
+++ datapipe/getdata.py
@@ -1,5 +1,6 @@
 # Read the wine-quality csv file from the URL
 import os
+import requests
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 csv_url =\
@@ -11,4 +12,8 @@ blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 local_file_name = 'landing_zone/raw_data.csv'
 # Create a blob client using the local file name as the name for the blob
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-blob_client.upload_blob(csv_url, overwrite=True)
+response = requests.get(csv_url)
+with open('./data.csv', 'wb') as f:
+    f.write(response.content)
+blob_client.upload_blob('./data.csv', overwrite=True)
+
